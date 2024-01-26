c30017d0d78da1262206fbef9d8ff3325a4bcada
James Nornubari <nornubariconfidence@gmail.com>
2024-01-26 15:00:28 +0100

    Updated Jenkinsfile

--- core/event/consume_event.py
+++ core/event/consume_event.py
@@ -1,5 +1,5 @@
 from core.helper.consumer_helper import consume_event
-from core.model.account_model import Disable_Enable_Account
+from core.model.account_model import DisableEnableAccount
 from core.utils.settings import settings
 from core.helper.account_helper import disable_or_enable_account
 from core.utils.init_log import logger
@@ -15,7 +15,7 @@ async def consume_disable_or_enable_account_event():
             logger.info('Received disable or enable account event.')          
             
             # Deserialize event
-            account_data = Disable_Enable_Account.deserialize(data=msg.value)
+            account_data = DisableEnableAccount.deserialize(data=msg.value)
 
             # Processing event
             logger.info('Processing event.')
--- core/model/account_model.py
+++ core/model/account_model.py
@@ -4,6 +4,6 @@ from pydantic import Field
 class AccountID(AvroBaseModel):
     id: str = Field(description='Id to id an account.')
 
-class Disable_Enable_Account(AccountID):
+class DisableEnableAccount(AccountID):
     disabled: bool = Field(description="Flag used to enable or disable an account.")
 
